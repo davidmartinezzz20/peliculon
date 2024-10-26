1cbb2b499aef68a07c82acc3dc100de0375e55d3
davidmartinezzz20 <opt1a.david.martinez@gmail.com>
2024-10-26 19:27:01 +0200

    writing next releases to a list variable

--- infoTransformer.py
+++ infoTransformer.py
@@ -1,14 +1,28 @@
 from takeInfo import filmsFile
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
+
+URL="https://yelmocines.es"
 
 with open(filmsFile, 'r', encoding='utf-8') as file:
     html_content = file.read()
 
 soup = BeautifulSoup(html_content, "html.parser")
 
-filmsNames = soup.find_all('div', class_='listProxEstreno releaseDate__item cf')
+filmsInfo = soup.find_all('article', class_='releaseDate')
+#filmsDates = soup.find_all('h3', class_='releaseDay')
 
-for filmName in filmsNames:
-    print(filmName.text)
+d = []
 
-## Taking all film titles
+for filmInfo in filmsInfo:
+
+    # Taking movie name, synopsis href and release date
+    filmName = filmInfo.find('figcaption').text.strip()
+    filmHref = filmInfo.find('a', href=True)
+    filmHref = URL+filmHref['href']
+    filmDate = filmInfo.find('h3', class_='releaseDay').text.strip()
+
+    # Write each movie to a list
+    line = {'Name':filmName, 'URL':filmHref, 'Release date':filmDate}
+    d.append(line)
+
+print(d)
